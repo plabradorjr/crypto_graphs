f5e2d64eef6128fb8e4cffc2fa7bd5b5233486e7
Pancho Labrador <plabradorjr@gmail.com>
2020-12-24 16:36:32 -0600

    added AppProvider and hooked up state

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+import {AppContext} from './AppProvider';
 
 const Bar = styled.div `
     display: grid;
     grid-template-columns: 180px auto 100px 100px;
 `
 
-export default function(){
-    return <Bar> 
-        <div>CrypotDash</div>
-        <div></div>
-        <div>Dashboard</div>    
-        <div>Settings</div>    
+const ControlButtonElem = styled.div`
+    cursor: pointer;
+    ${props => props.active && css`
+    text-shadow: 0px 0px 20px #55efc4
+    `}
+`
+function toProperCase(lower){
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+  }
 
-    </Bar>
+function ControlButton({name, active}){
+    return (
+        <AppContext.Consumer>
+            {({page, setPage}) => (
+            <ControlButtonElem
+                active={page === name}
+                onClick={()=> setPage(name)}
+            >
+            {toProperCase(name)}
+            </ControlButtonElem>
+            )}
+        </AppContext.Consumer>
+        
+    )
+};
+
+export default function(){
+    return (
+        <Bar> 
+            <div>CryptoDash</div>
+            <div></div>
+            <ControlButton name="dashboard" />  
+            <ControlButton name="settings" />
+        </Bar>
+    );
 }
